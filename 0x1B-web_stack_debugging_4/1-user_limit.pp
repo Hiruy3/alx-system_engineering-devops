ab9e1185d8815ee93b7429f9d15d4d5ee5e87356
Hiruy Melaku <95427259+Hiruy3@users.noreply.github.com>
2023-05-08 23:30:42 +0300

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,5 +0,0 @@
-# login as holberton and open any number of files without any error message.
-exec {'open any files without limits':
-  command => 'sed -rie \'s/(holberton (hard|soft) nofile).*/\1 1000/gi\' /etc/security/limits.conf',
-  path    => 'usr/bin/:/bin/'
-}
